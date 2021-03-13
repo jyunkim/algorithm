adb4a07318fcaf520af5e8a8e31fc12a16e45503
JyunKim <kann1997@naver.com>
2021-03-13 11:54:25 +0900

    18

--- baekjoon/정렬/2108_통계학.py
+++ baekjoon/정렬/2108_통계학.py
@@ -1,5 +1,6 @@
 # input보다 sys가 훨 빠름
 import sys
+from collections import Counter
 
 n = int(sys.stdin.readline())
 nums = [int(sys.stdin.readline()) for i in range(n)]
@@ -9,20 +10,30 @@ s = round(sum(nums) / len(nums))
 nums.sort()
 j = nums[len(nums) // 2]
 
-# dictinary 생성 -> zip 이용
-d = {}
-for num in nums:
-    if num not in d:
-        d[num] = 0
-    else:
-        d[num] += 1
-cs = [k for k, v in d.items() if v == max(d.values())]
-if len(cs) == 1:
-    c = cs[0]
-else:
-    c = cs[1]
+# d = {}
+# for num in nums:
+#     if num not in d:
+#         d[num] = 0
+#     else:
+#         d[num] += 1
+# cs = [k for k, v in d.items() if v == max(d.values())]
+# if len(cs) == 1:
+#     c = cs[0]
+# else:
+#     c = cs[1]
+
 # max(d, key=d.get) -> value값이 가장 큰 key값
-# collections의 Counter 사용가능
+# dictinary 생성 -> zip 이용
+
+cd = Counter(nums)  # dictionary 형태 {'a': 1}
+cs = cd.most_common()  # 리스트 안의 튜플 형태 [('a', 1)]
+if len(cs) == 1:
+    c = cs[0][0]
+else:
+    if cs[0][1] == cs[1][1]:
+        c = cs[1][0]
+    else:
+        c = cs[0][0]
 
 r = max(nums) - min(nums)
 
