33116735e86e6cabf0dda6d74fc95222b1822467
JyunKim <kann1997@naver.com>
2021-04-27 01:36:24 +0900

    1

--- programmers/해시/완주하지못한선수.py
+++ programmers/해시/완주하지못한선수.py
@@ -1,9 +1,19 @@
-def solution(participant, completion):
-    participant.sort()
-    completion.sort()
+# completion에 있는 요소를 participant에서 remove => O(n^2)
+# 해시 사용 => O(n) average
 
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-    return participant[-1]
-    
+def solution(participant, completion):
+    part = to_dict(participant)
+    comp = to_dict(completion)
+    for k, v in part.items():
+        if v != comp.get(k):
+            return k
+
+
+def to_dict(arr):
+    dic = {}
+    for a in arr:
+        if a in dic:
+            dic[a] += 1
+        else:
+            dic[a] = 1
+    return dic
