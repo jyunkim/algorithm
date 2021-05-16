f14b24af5424852439b0f104010c37571dd3a4ca
JyunKim <kann1997@naver.com>
2021-05-16 23:59:31 +0900

    25

---
+++ baekjoon/스택/10828_스택.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+stk = []
+
+for _ in range(n):
+    cmd = sys.stdin.readline().strip()  # 끝에 개행문자 제거
+    a = cmd.split()
+    if len(a) > 1:
+        stk.append(a[1])
+    else:
+        if cmd == 'pop':
+            if stk:
+                print(stk.pop())
+            else:
+                print(-1)
+        elif cmd == 'size':
+            print(len(stk))
+        elif cmd == 'empty':
+            if stk:
+                print(0)
+            else:
+                print(1)
+        elif cmd == 'top':
+            if stk:
+                print(stk[-1])
+            else:
+                print(-1)
