725151647d8b184ce24dcf2abf4655b24271214d
JyunKim <kann1997@naver.com>
2021-05-26 23:47:37 +0900

    6

--- programmers/정렬/가장큰수.py
+++ programmers/정렬/가장큰수.py
@@ -1,16 +1,6 @@
+# 자릿수 별 정렬 => 문자열 변환 후 정렬
 def solution(numbers):
-    answer = ''
-    
-    if sum(numbers) == 0:
-        return "0"
-
-    #for i in range(len(numbers)):
-    #    numbers[i] = str(numbers[i])
-    numbers = sorted(map(lambda x: str(x), numbers), key = lambda x: x*4)
-                    #([str(x) for x in numbers])
-    while numbers:
-        answer += numbers.pop()
-    #answer = ''.join(numbers)
-    
-    return answer
-    #return str(int(answer))
+    # 3, 30, 34 -> 30, 3, 34
+    arr = sorted(map(str, numbers), key=lambda x: x*3, reverse=True)
+    # '00' -> '0'
+    return str(int(''.join(arr)))
